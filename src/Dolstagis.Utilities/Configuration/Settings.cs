0002633873975327f4f7d0632f344fb5173c6904
James McKay <code@jamesmckay.net>
2015-02-17 07:18:52 +0000

    This is what our API will look like.

---
+++ src/Dolstagis.Utilities/Configuration/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dolstagis.Utilities.Configuration
+{
+    public static class Settings
+    {
+        private static readonly IList<ISettingsSource> _sources
+            = new List<ISettingsSource>();
+
+        public static TSettings Get<TSettings>()
+        {
+            return default(TSettings);
+        }
+
+        public static IList<ISettingsSource> Sources
+        {
+            get { return _sources; }
+        }
+    }
+}
